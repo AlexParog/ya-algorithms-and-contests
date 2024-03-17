bbf511cd23b71a1fbd2547b1354e3fc525824e22
alexanderbibik <sbibik2000@mail.ru>
2024-03-17 22:18:04 +0300

    added section № 2, task C

---
+++ src/main/java/org/parog/section2/TaskC.java
@@ -0,0 +1,65 @@
+package org.parog.section2;
+
+import java.io.*;
+import java.util.Arrays;
+
+/**
+ * На столе лежали две одинаковые верёвочки целой положительной длины.
+ * Петя разрезал одну из верёвочек на N частей, каждая из которых имеет целую положительную длину, так что на столе стало
+ * N+1 верёвочек. Затем в комнату зашла Маша и взяла одну из лежащих на столе верёвочек. По длинам оставшихся на столе N
+ * верёвочек определите, какую наименьшую длину может иметь верёвочка, взятая Машей.
+ */
+public class TaskC {
+
+    private static final String INPUT_FILE_PATH = "src/main/resources/input.txt";
+
+    private static final String OUTPUT_FILE_PATH = "src/main/resources/output.txt";
+
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(INPUT_FILE_PATH));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(OUTPUT_FILE_PATH))) {
+            int numberRopes = Integer.parseInt(reader.readLine());
+
+            int[] ropes = Arrays.stream(reader.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+            String result = String.valueOf(getMinimalRope(ropes));
+
+            writer.write(result);
+        }
+    }
+
+    /**
+     * В задаче две ситуации:
+     * 1) Когда одна (максимальная) из двух изначальных веревок лежит среди оставшихся и необходимо вернуть
+     * разницу между ней и суммой остальных.
+     * 2) Когда вторую изначальную веревку взяла Маша, тогда возвращаем сумму длин всех веревок на столе.
+     *
+     * @param ropes длины оставшихся веревочек
+     * @return наименьшая длина, которую может иметь верёвочка, взятая Машей
+     */
+    public static int getMinimalRope(int[] ropes) {
+        int maxRope = Arrays.stream(ropes)
+                .max()
+                .getAsInt();
+
+        int maxRopeIndex = -1;
+        // находим индекс максимального элемента
+        for (int i = 0; i < ropes.length; i++) {
+            if (ropes[i] == maxRope) {
+                maxRopeIndex = i;
+                break;
+            }
+        }
+        // находим сумму без учета максимальной веревки
+        int withoutMaxRope = 0;
+        for (int i = 0; i < ropes.length; i++) {
+            if (i != maxRopeIndex) {
+                withoutMaxRope += ropes[i];
+            }
+        }
+
+        return (maxRope - withoutMaxRope > 0) ? (maxRope - withoutMaxRope) : (maxRope + withoutMaxRope);
+    }
+}
