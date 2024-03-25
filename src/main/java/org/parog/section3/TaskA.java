8a6c47602619ee1eb1721e249890d9428e517f26
alexanderbibik <sbibik2000@mail.ru>
2024-03-25 16:35:43 +0300

    added section № 3, tasks A, B

---
+++ src/main/java/org/parog/section3/TaskA.java
@@ -0,0 +1,69 @@
+package org.parog.section3;
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Костя успешно прошел собеседование и попал на стажировку в отдел разработки сервиса «Музыка».
+ * Конкретно ему поручили такое задание — научиться подбирать плейлист для группы друзей, родственников или коллег.
+ * При этом нужно подобрать такой плейлист, в который входят исключительно нравящиеся всем членам группы песни.
+ * Костя очень хотел выполнить это задание быстро и качественно, но у него не получается. Помогите ему написать программу,
+ * которая составляет плейлист для группы людей.
+ */
+public class TaskA {
+
+    private static final String INPUT_FILE_PATH = "src/main/resources/input.txt";
+
+    private static final String OUTPUT_FILE_PATH = "src/main/resources/output.txt";
+
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(INPUT_FILE_PATH));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(OUTPUT_FILE_PATH))) {
+            int members = Integer.parseInt(reader.readLine());
+            Set<String> songsAllMembers = new HashSet<>();
+
+            // если один человек в группе
+            if (members == 1) {
+                int numberOfSongs = Integer.parseInt(reader.readLine());
+                String[] songs = reader.readLine().split(" ");
+                songsAllMembers.addAll(List.of(songs));
+
+                writer.write(String.valueOf(songsAllMembers.size()));
+                writer.newLine();
+                writer.write(printSongs(songsAllMembers));
+
+                return;
+            }
+
+            for (int i = 0; i < members; i++) {
+                int numberOfMemberSongs = Integer.parseInt(reader.readLine());
+                Set<String> memberFavSongs = new HashSet<>(List.of(reader.readLine().split(" ")));
+
+                if (i == 0) {
+                    songsAllMembers.addAll(memberFavSongs);
+                } else {
+                    songsAllMembers.retainAll(memberFavSongs);
+                }
+            }
+
+            writer.write(String.valueOf(songsAllMembers.size()));
+            writer.newLine();
+            writer.write(printSongs(songsAllMembers));
+        }
+    }
+
+    /**
+     * Преобразуем Set<String> в строку, с условием лексикографического порядка
+     *
+     * @param songsAllMembers любимые песни всех участников
+     * @return песни в лексикографическом порядке
+     */
+    public static String printSongs(Set<String> songsAllMembers) {
+        return songsAllMembers.stream()
+                .sorted()
+                .collect(Collectors.joining(" "));
+    }
+}
---
+++ src/main/java/org/parog/section3/TaskB.java
@@ -0,0 +1,40 @@
+package org.parog.section3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Задано две строки, нужно проверить, является ли одна анаграммой другой.
+ * Анаграммой называется строка, полученная из другой перестановкой букв.
+ */
+public class TaskB {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String firstRow = scanner.nextLine().toLowerCase();
+        String secondRow = scanner.nextLine().toLowerCase();
+
+        if (firstRow.length() == secondRow.length()) {
+            System.out.println(isAnagram(firstRow, secondRow));
+        } else {
+            System.out.println("NO");
+        }
+    }
+
+    /**
+     * Является ли строка анаграммой
+     *
+     * @param firstRow  первая строка
+     * @param secondRow вторая строка
+     * @return YES - анаграмма, NO - разные строки
+     */
+    public static String isAnagram(String firstRow, String secondRow) {
+        char[] charsFirstRow = firstRow.toCharArray();
+        Arrays.sort(charsFirstRow);
+
+        char[] charsSecondRow = secondRow.toCharArray();
+        Arrays.sort(charsSecondRow);
+
+        return Arrays.equals(charsFirstRow, charsSecondRow) ? "YES" : "NO";
+    }
+}
