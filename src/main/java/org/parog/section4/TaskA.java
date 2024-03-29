949a3d64154c41ffaf9da21a3d431cff3cc411f8
alexanderbibik <sbibik2000@mail.ru>
2024-03-29 14:12:46 +0300

    added section № 4, tasks A, B

--- src/main/java/org/parog/section4/TaskA.java
+++ src/main/java/org/parog/section4/TaskA.java
@@ -1,4 +1,58 @@
 package org.parog.section4;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Дан массив из N целых чисел. Все числа от −10^9 до 10^9.
+ * <p>
+ * Нужно уметь отвечать на запросы вида “Cколько чисел имеют значения от L до R?”.
+ */
 public class TaskA {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] mass = new int[n];
+        for (int i = 0; i < n; i++) {
+            mass[i] = scanner.nextInt();
+        }
+        Arrays.sort(mass);
+
+        int k = scanner.nextInt();
+        for (int i = 0; i < k; i++) {
+            int leftBorder = scanner.nextInt();
+            int rightBorder = scanner.nextInt();
+            int indexOfLeftBorder = binarySearchFirstX(leftBorder, mass);
+            int indexOfRightBorderPlusOne = binarySearchFirstX(rightBorder + 1, mass);
+            System.out.print(indexOfRightBorderPlusOne - indexOfLeftBorder + " ");
+        }
+    }
+
+    /**
+     * Выполняет бинарный поиск в отсортированном массиве для нахождения индекса
+     * первого вхождения числа x. Если число не найдено, возвращает индекс
+     * ближайшего числа, которое больше или равно x.
+     *
+     * @param x   Искомое число.
+     * @param arr Отсортированный массив чисел.
+     * @return Индекс первого вхождения числа x или индекс ближайшего числа,
+     * которое больше или равно x.
+     */
+    public static int binarySearchFirstX(int x, int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+        int answer = -1;
+        while (left <= right) {
+            int m = (left + right) / 2;
+            if (arr[m] >= x) {
+                right = m - 1;
+                answer = m;
+            } else {
+                left = m + 1;
+            }
+        }
+        if (right == -1) return 0;
+        if (left == arr.length) return arr.length;
+        return answer;
+    }
 }
--- src/main/java/org/parog/section4/TaskB.java
+++ src/main/java/org/parog/section4/TaskB.java
@@ -1,4 +1,68 @@
 package org.parog.section4;
 
+import java.util.Scanner;
+
+/**
+ * Поле в игре в одномерный морской бой имеет размеры 1×n. Ваша задача — найти такое максимальное k, что на поле можно
+ * расставить один корабль размера 1×k, два корабля размера 1×(k−1), …, k кораблей размера 1×1, причем корабли, как и в
+ * обычном морском бое, не должны касаться друг друга и пересекаться.
+ */
 public class TaskB {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextLong();
+        long maxShips = findMaxShips(n);
+        System.out.println(maxShips);
+    }
+
+    /**
+     * Метод для поиска максимального числа кораблей, которое можно разместить на поле.
+     *
+     * @param n количество клеток на поле
+     * @return максимальное количество кораблей, которое можно разместить
+     */
+    public static long findMaxShips(long n) {
+        long left = 0;
+        long right = n;
+        long answer = -1;
+
+        // Бинарный поиск для определения максимального числа кораблей
+        while (left <= right) {
+            long middle = (left + right) / 2;
+            if (isValidConfiguration(n, middle)) {
+                left = middle + 1;
+                answer = middle;
+            } else {
+                right = middle - 1;
+            }
+        }
+
+        return answer;
+    }
+
+    /**
+     * Метод для проверки возможности размещения кораблей на поле для заданного числа кораблей.
+     *
+     * @param availableCells количество клеток на поле
+     * @param shipCount      количество кораблей, которые необходимо разместить
+     * @return {@code true}, если размещение возможно, {@code false} в противном случае
+     */
+    public static boolean isValidConfiguration(long availableCells, long shipCount) {
+        availableCells -= shipCount;
+        shipCount -= 1;
+        long shipLength = 2;
+
+        // Проверяем возможность размещения кораблей на поле
+        while (shipCount > 0) {
+            availableCells -= (shipCount + 1) * shipLength;
+            if (availableCells < 0) {
+                return false;
+            }
+            shipCount -= 1;
+            shipLength += 1;
+        }
+
+        return availableCells >= 0;
+    }
 }
