1756a442c1248f73682c79cdaf89fd8ec94fe9ed
alexanderbibik <sbibik2000@mail.ru>
2024-07-19 11:44:35 +0300

    algorithm_training_1.section1: Come back later AmbulanceTaskE

---
+++ src/main/java/org/parog/algorithm_training_1/section1/AmbulanceTaskE.java
@@ -0,0 +1,10 @@
+package org.parog.algorithm_training_1.section1;
+
+/**
+ * E. Скорая помощь
+ */
+public class AmbulanceTaskE {
+    public static void main(String[] args) {
+        //TODO: слоооожнаа, вернуться позже
+    }
+}
